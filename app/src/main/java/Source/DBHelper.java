b01cbb015320927741f447fb60ff5a12e8af65fa
Roman Asadov <roma-asadov@mail.ru>
2018-12-11 01:15:08 +0300

    переименовал название базы данных и таблицы

--- app/src/main/java/Source/DBHelper.java
+++ app/src/main/java/Source/DBHelper.java
@@ -7,10 +7,10 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBHelper extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "contactDb";
-    public static final String TABLE_CONTACTS = "contacts";
+    public static final String DATABASE_NAME = "time_manager_db";
+    public static final String TABLE_CONTACTS = "tasks";
 
-    public static final String KEY_ID = "_id";
+    public static final String KEY_ID = "task_id";
     public static final String KEY_QUESTION = "question";
     public static final String KEY_TIME = "time";
 
