3a1b4b95406c08bf5cd6642eb257d781502d4dd1
Dashusha <losevadasha258@gmail.com>
2018-12-13 00:58:33 +0300

    i tryed made disign

--- app/src/main/java/Source/DBHelper.java
+++ app/src/main/java/Source/DBHelper.java
@@ -3,12 +3,12 @@ package Source;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.TextView;
 
 public class DBHelper extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "time_manager_db";
-
 
     public DBHelper(Context context) {
         super(context,DATABASE_NAME, null, DATABASE_VERSION);
@@ -23,11 +23,14 @@ public class DBHelper extends SQLiteOpenHelper {
                 "task_priority integer" +
                 ")");
 
+
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("drop table if exists tasks");
         onCreate(sqLiteDatabase);
+
     }
 }
