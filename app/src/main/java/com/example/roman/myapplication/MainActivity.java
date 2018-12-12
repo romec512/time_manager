7797ce8e30135fcc96a41486e83a840a06ce3325
JahongirOlolo <jahon-0000@mail.ru>
2018-12-12 22:52:12 +0300

    Кнопки

--- app/src/main/java/com/example/roman/myapplication/MainActivity.java
+++ app/src/main/java/com/example/roman/myapplication/MainActivity.java
@@ -5,6 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CalendarView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +18,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView icon = new ImageView(this);
+        icon.setImageResource(R.drawable.add);
+        final FloatingActionButton fab = new FloatingActionButton.Builder(this).setContentView(icon).build();
+
+        SubActionButton.Builder builder = new SubActionButton.Builder(this);
+
+        ImageView addIcon = new ImageView(this);
+        addIcon.setImageResource(R.drawable.add_green);
+        SubActionButton add = builder.setContentView(addIcon).build();
+
+        ImageView playIcon = new ImageView(this);
+        playIcon.setImageResource(R.drawable.play);
+        SubActionButton play = builder.setContentView(playIcon).build();
+
+        ImageView removeIcon = new ImageView(this);
+        removeIcon.setImageResource(R.drawable.remove);
+        SubActionButton remove = builder.setContentView(removeIcon).build();
+
+
+
+        final FloatingActionMenu fam = new FloatingActionMenu.Builder(this)
+                .addSubActionView(add)
+                .addSubActionView(play)
+                .addSubActionView(remove)
+                .attachTo(fab)
+                .build();
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Вы нажали Add", Toast.LENGTH_SHORT).show();
+                fam.close(true);
+            }
+        });
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Вы нажали Play", Toast.LENGTH_SHORT).show();
+                fam.close(true);
+            }
+        });
+
+        remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Вы нажали remove", Toast.LENGTH_SHORT).show();
+                fam.close(true);
+            }
+        });
+
         CalendarView cw = (CalendarView) findViewById(R.id.calendarView);
         cw.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
