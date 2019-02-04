ad7a905f90f32d0947f6f036ace3e6f7fb2be3f1
Dashusha <losevadasha258@gmail.com>
2019-02-05 00:15:40 +0300

    создала стреничку deadline

---
+++ app/src/main/java/com/example/roman/myapplication/ActivityDeadline.java
@@ -0,0 +1,13 @@
+package com.example.roman.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityDeadline extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deadline);
+    }
+}
